d4ced54de104b4f7b3a1333defcfafef013f81bb
Josphatmyaya <Josphatmyaya@hotmail.com>
2025-08-13 03:36:07 +0200

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,12 +1,12 @@
 export const getGlobalData = () => {
-  const name = process.env.BLOG_NAME
-    ? decodeURI(process.env.BLOG_NAME)
+  const name = process.env.MYAYA_TECHNOLOGIES
+    ? decodeURI(process.env.MYAYA_TECHNOLOGIES)
     : 'Jay Doe';
   const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.BLOG_TITLE)
+    ? decodeURI(process.env.MYAYA_TECHNOLOGIES)
     : 'Next.js Blog Theme';
   const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
+    ? decodeURI(process.env.HOT_PRO)
     : 'All rights reserved.';
 
   return {
